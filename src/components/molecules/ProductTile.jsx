8d31e9f92c0f5b164dcc4bfd8960822580e92427
Emil Beski <beskiemil@gmail.com>
2023-07-22 21:13:03 +0200

    changed sizing of ProductTile

--- src/components/molecules/ProductTile.jsx
+++ src/components/molecules/ProductTile.jsx
@@ -4,7 +4,7 @@ import Button from "components/atoms/Button";
 
 const ProductTile = ({ img, name, description }) => {
   return (
-    <div className="flex w-96 flex-col items-center justify-center gap-3 bg-greenGrey bg-opacity-90 px-14 py-10 text-white">
+    <div className="flex w-80 flex-col items-center justify-center gap-3 bg-greenGrey bg-opacity-90 px-10 py-10 text-white md:w-96">
       <img
         src={"http://localhost:3000/" + img}
         className="h-64 w-64 rounded-[50px] object-cover object-center"
